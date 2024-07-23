b53e80674765c4ff2cc72aaf48fca57ca2ca00dc
nick-0101 <54145857+nick-0101@users.noreply.github.com>
2024-07-22 22:09:56 -0400

    ++

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { default as QuaggaScanner } from './QuaggaScanner.vue';
-import { App as AppType } from 'vue';
-
-export { QuaggaScanner };
-declare const _default: {
-    install: (app: AppType) => void;
-};
-export default _default;
